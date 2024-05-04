e8e8b9f0c086ba62334a4e3f0d39d2102182325a
Rishikesh Gautam <rishikesh2059@gmail.com>
2024-05-04 13:37:38 +0545

    added background color changing project

--- 02project/src/App.jsx
+++ 02project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Card from './components/Card'
 import './App.css'
 
---
+++ 03project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  // function changeColor(color) {
+  //   setColor(color)
+  // }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor('red')} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'red'}}>Red</button>
+          <button
+            onClick={() => setColor('green')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'green'}}>Green</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
