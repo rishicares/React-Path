24bceebd741c38db4a0d85460820a2dfdfec9785
Rishikesh Gautam <rishikesh2059@gmail.com>
2024-05-06 23:57:24 +0545

    added space in password

--- 04project/src/App.jsx
+++ 04project/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     if(numberAllowed) str +="0123456789"
-    if(charAllowed) str +="!@#$%^&*()_+"
+    if(charAllowed) str +="!@#$%^ &*()_+"
 
     for(let i=0; i <length; i++){
       const char = Math.floor(Math.random() * str.length + 1)
