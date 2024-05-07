72bbcaf993669b14dce9275729436fa1e8609ae0
Rishikesh Gautam <rishikesh2059@gmail.com>
2024-05-07 22:33:45 +0545

    improved overall UI

--- 04project/src/App.jsx
+++ 04project/src/App.jsx
@@ -35,8 +35,8 @@ function App() {
   
   return (
 
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className="text-white text-center my-3 ">Password Generator</h1>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-600 text-orange-500">
+      <h1 className="text-white text-center my-3 text-xl font-bold  ">PASSWORD GENERATOR</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
           type="text"
