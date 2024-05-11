5e98bea2259cc2d25316a65c4552796d95d8f7ab
Rishikesh Gautam <rishikesh2059@gmail.com>
2024-05-11 22:46:22 +0545

    added home content

--- 06project/src/components/Footer/Footer.jsx
+++ 06project/src/components/Footer/Footer.jsx
@@ -72,7 +72,7 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        <a href="https://rishikesh.com/" className="hover:underline">
                             hiteshchoudhary
                         </a>
                         . All Rights Reserved.
