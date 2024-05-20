9253a70a6de41c86b9b7c7b0cccd405fff54994c
Rishikesh Gautam <rishikesh2059@gmail.com>
2024-05-20 23:17:34 +0545

    footer attached to bottom

--- 06project/src/components/Footer/Footer.jsx
+++ 06project/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
+        <main className='min-h-screen flex flex-col justify-between'><section className='flex-grow'></section><footer className="bg-white border-y">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
@@ -158,5 +158,6 @@ export default function Footer() {
                 </div>
             </div>
         </footer>
+        </main>
     );
 }
